c4a060094d7c91a57aaa1e5e3609be248a74056a
Layora-cpu <layorahijabs@gmail.com>
2026-06-26 18:38:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+// Welcome Message
+console.log("Welcome to Layora");
 
+// Navbar Shadow on Scroll
+window.addEventListener("scroll", function () {
+
+const navbar = document.querySelector(".navbar");
+
+if (window.scrollY > 50) {
+navbar.style.boxShadow = "0 10px 25px rgba(0,0,0,0.15)";
+} else {
+navbar.style.boxShadow = "0 5px 15px rgba(0,0,0,0.08)";
+}
+
+});
+
+// Smooth Fade Animation
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver((entries) => {
+
+entries.forEach(entry => {
+
+if(entry.isIntersecting){
+
+entry.target.style.opacity = "1";
+entry.target.style.transform = "translateY(0px)";
+
+}
+
+});
+
+});
+
+cards.forEach(card=>{
+
+card.style.opacity="0";
+card.style.transform="translateY(40px)";
+card.style.transition="0.8s";
+
+observer.observe(card);
+
+});
