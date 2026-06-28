deaf5e7d3f39aa47a48386551774cd9093cad301
Layora-cpu <layorahijabs@gmail.com>
2026-06-28 22:28:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,3 +41,30 @@ card.style.transition="0.8s";
 observer.observe(card);
 
 });
+const topBtn=document.getElementById("topBtn");
+
+window.onscroll=function(){
+
+if(document.body.scrollTop>300||document.documentElement.scrollTop>300){
+
+topBtn.style.display="block";
+
+}else{
+
+topBtn.style.display="none";
+
+}
+
+};
+
+topBtn.onclick=function(){
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+};
